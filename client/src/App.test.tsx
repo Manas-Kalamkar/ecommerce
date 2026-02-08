5c5c4be4069d05843c0d3386bef5381c2e53fe16
Manas <manaskalamkar910@gmail.com>
2026-02-08 23:47:54 +0530

    test: remove unused default App test

--- client/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import App from './App';
-import { describe ,expect,it} from 'vitest';
-import {render,screen} from '@testing-library/react'
-describe("App",()=>{
-    it('should have manas', ()=>{
-        render(<App />);
-        expect(screen.getByText("Welcome to Manass React E-Commerce Website")).toBeInTheDocument();
-    })
-})
