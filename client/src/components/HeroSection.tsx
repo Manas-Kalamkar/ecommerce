13fc86ca261de7003ec4238bacebcef438bee8da
Manas <manaskalamkar910@gmail.com>
2026-02-09 02:10:02 +0530

    get image from public folder "/" missing

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -30,7 +30,7 @@ const name : string = myData;
                     </div>
                     <div className="hero-section-image">
                         <figure>
-                            <img src='images/hero.jpg' alt="hero-section-photo" className='img-style' />
+                            <img src='/images/hero.jpg' alt="hero-section-photo" className='img-style' />
                         </figure>
                     </div>
                 </div>
